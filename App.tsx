d8920b50b4baad49f35e90ba8cf9dd94b65c04e9
Mohsin Ansari <mohsinansari0705@gmail.com>
2025-11-24 18:19:34 +0530

    safe area bug fix

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 // React is used in JSX, but not in this file's direct code
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from './lib/ThemeContext';
 import { AuthProvider } from './lib/AuthContext';
 import { NavigationProvider } from './lib/NavigationContext';
@@ -8,13 +9,15 @@ import { Colors } from './lib/colors';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <AuthProvider>
-        <NavigationProvider>
-          <AppNavigator />
-          <StatusBar style="dark" backgroundColor={Colors.softLightGrey} />
-        </NavigationProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <NavigationProvider>
+            <AppNavigator />
+            <StatusBar style="dark" translucent={false} backgroundColor={Colors.softLightGrey} />
+          </NavigationProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
